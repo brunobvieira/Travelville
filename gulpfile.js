d7c9b140761d3c75a41c4b3941be7713a58d61e4
Bruno Beserra VIeira <brunobeserravieira@gmail.com>
2018-08-18 15:10:07 -0300

    - Inicio da Aula

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,46 @@
 'use strict';
 const gulp = require('gulp');
 const sass = require('gulp-sass');
+const rename = require('gulp-rename');
+
+
+// Src Sass files for compile
+const srcSassFiles = 'assets/sass';
+const arrSrcSassFiles = [
+    srcSassFiles + '/*.scss',
+    srcSassFiles + '/components/*.scss',
+    srcSassFiles + '/components/forms/*.scss'
+];
+
+// Src output for css files
+const srcCssFiles = 'assets/css';
+
+// Options for development
+const sassDevOptions = {
+    outputStyle: 'expanded'
+};
+
+// Options for production
+const sassProdOptions = {
+    outputStyle: 'compressed'
+};
 
 //COMPILE CSS
-gulp.task('sass', function () {
-    return gulp.src(['assets/sass/*.scss'])
-        .pipe(sass().on('error', sass.logError))
-        .pipe(gulp.dest('assets/css'));
+gulp.task('sassdev', function () {
+    return gulp.src(arrSrcSassFiles)
+        .pipe(sass(sassDevOptions).on('error', sass.logError))
+        .pipe(gulp.dest(srcCssFiles));
 });
 
-gulp.task('serve', ['sass'], function () {
-    gulp.watch(['assets/sass/*.scss'], ['sass']);
+gulp.task('sassprod', function () {
+    return gulp.src(arrSrcSassFiles)
+        .pipe(sass(sassProdOptions).on('error', sass.logError))
+        .pipe(rename({extname: '.min.css'}))
+        .pipe(gulp.dest(srcCssFiles));
 });
 
+gulp.task('watch', function () {
+    gulp.watch(arrSrcSassFiles, ['sassdev']);
+});
 
-gulp.task('default', ['serve']);
+gulp.task('default', ['watch']);
